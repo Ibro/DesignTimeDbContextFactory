f25f4011c36c9f2153f6616f9a736aca43e0de44
Ibro <ibro@ibro.me>
2017-08-21 14:58:18 +0200

    adding implementation of IDesignTimeDbContextFactory

---
+++ Web/DesignTimeDbContextFactory.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Web
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<CodingBlastDbContext>
+    {
+        public CodingBlastDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<CodingBlastDbContext>();
+
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);       
+
+            return new CodingBlastDbContext(builder.Options);
+        }
+    }
+}
