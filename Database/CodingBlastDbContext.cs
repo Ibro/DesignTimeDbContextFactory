288abefa437485b9b5df6d7332e094d13193b50d
Ibro <ibro@ibro.me>
2017-08-21 14:54:25 +0200

    Adding DbContext and DbSet

---
+++ Database/CodingBlastDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Database
+{
+    public class CodingBlastDbContext: DbContext
+    {
+        public CodingBlastDbContext(DbContextOptions<CodingBlastDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
